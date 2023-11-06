5a80fe0d0abd16bddf159301490ff979ee56dec2
leticiakaorism <146925752+leticiakaorism@users.noreply.github.com>
2023-11-06 16:52:33 +1100

    Added JavaScript Form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello");
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchFormInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchFormInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
