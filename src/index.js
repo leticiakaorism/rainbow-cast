56b1b79a745b749e663f615883fb2203e35cb9d4
leticiakaorism <146925752+leticiakaorism@users.noreply.github.com>
2023-11-07 07:48:52 +1100

    Added JavaScript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response);
+}
+
+function searchCity(city) {
+  let apiKey = "232do9057abbbe75ece0tff744ed37df";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchFormInput.value;
+  searchCity(searchFormInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
