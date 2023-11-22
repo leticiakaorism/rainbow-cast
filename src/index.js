1978e319e5e47a6785fdc4c97052a57b4e59c8c1
leticiakaorism <146925752+leticiakaorism@users.noreply.github.com>
2023-11-23 08:16:52 +1100

    Implemented Real Life Data using API

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKeyForecast = "232do9057abbbe75ece0tff744ed37df";
   let apiUrlForecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKeyForecast}`;
@@ -64,13 +71,14 @@ function getForecast(city) {
 function displayForecast(response) {
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tue</div>
-        <div class="weather-forecast-icon">${day.condition.icon_url}</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img class="weather-forecast-icon" src="${day.condition.icon_url}">
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}º</strong>
@@ -81,6 +89,7 @@ function displayForecast(response) {
         </div>
       </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
